f65fd93d058bfa8f149640bfa0c50326a0fb1815
JGordon <JGordon@CLUKHM008365.corp.group.local>
2018-02-27 09:12:56 +0000

    completed routing logic

---
+++ src/main/webapp/app/app-state-manager.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function () {
+
+    angular.module('accountApp').config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/dashboard");
+
+        $stateProvider.state("dashboard", {
+            url: "/dashboard",
+            templateUrl: "app/feature/dashboard/dashboard.html"
+        }).state("account", {
+                url: "/account",
+                templateUrl: "app/feature/account/account.html"
+        })
+    });
+}());
