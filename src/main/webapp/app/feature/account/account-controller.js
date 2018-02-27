60fa637463522b02d03bb3708c0be3813c9f7c1b
JGordon <JGordon@CLUKHM008365.corp.group.local>
2018-02-27 10:51:17 +0000

    added service package

--- src/main/webapp/app/feature/account/account-controller.js
+++ src/main/webapp/app/feature/account/account-controller.js
@@ -1,34 +1,21 @@
 (function() {
 
-    var AccountController =  function() {
+    var AccountController =  function(accountService, $log) {
         
     	var vm = this;
         
         vm.isHidden = false;
         
-        vm.accounts = [
-        	{'firstName': 'Declan', 'secondName': 'Cordial','accountNumber': '1234'},
-        	{'firstName': 'Niall','secondName': 'Mulready','accountNumber': '1235'},
-        	{'firstName': 'Ian','secondName': 'Cunningham','accountNumber': '1236'},
-        	{'firstName': 'Brendan','secondName': 'Greene','accountNumber': '1237'},
-        	{'firstName': 'Nicholas','secondName': 'Tsang','accountNumber': '1238'},
-        	{'firstName': 'Sean','secondName': 'McGee','accountNumber': '1239'},
-        	{'firstName': 'Anthony','secondName': 'Shannon','accountNumber': '1240'},
-        	{'firstName': 'Conor','secondName': 'Harney','accountNumber': '1241'},
-        	{'firstName': 'Decland','secondName': 'Horlick','accountNumber': '1242'},
-        	{'firstName': 'Joshua','secondName': 'Sherwood','accountNumber': '1243'},
-        	{'firstName': 'Bau','secondName': 'Nguyen','accountNumber': '1243'},
-        	{'firstName': 'Kennedy','secondName': 'Bowers','accountNumber': '1243'},
-        	{'firstName': 'Daleel','secondName': 'Ramjan','accountNumber': '1243'},
-        	{'firstName': 'Ismael','secondName': 'Omar','accountNumber': '1243'}
-        	];
+        vm.accounts = accountService.accounts;
         
         vm.hideTable = function()
         {
         	vm.isHidden = !vm.isHidden
+        	$log.log("In the account controller the value of accounts is ");
+        	$log.log(JSON.stringify(vm.accounts));
         };
             
     };
 
-    angular.module('accountApp').controller('accountController', [AccountController]);
+    angular.module('accountApp').controller('accountController', ['accountService','$log', AccountController]);
 }());
